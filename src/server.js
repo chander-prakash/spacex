4a06d6ff9552c8fbe213b5717c0b13fd214f1ec8
Chander Prakash <chander@cldcvr.com>
2020-08-17 17:21:55 +0530

    Heroku showing nginx error 404, commenting caching server code

--- src/server.js
+++ src/server.js
@@ -3,18 +3,18 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-const staticFileOptions = {
-    etag: true,
-    maxage: 360000, // 1hr
-    redirect: true,
-    setHeaders: (res) => {
-        res.set({
-            'x-timestamp': Date.now()
-        })
-    }
+// const staticFileOptions = {
+//     etag: true,
+//     maxage: 360000, // 1hr
+//     redirect: true,
+//     setHeaders: (res) => {
+//         res.set({
+//             'x-timestamp': Date.now()
+//         })
+//     }
 
-}
-app.use(express.static(path.join(__dirname, '..', 'build'), staticFileOptions))
+// }
+app.use(express.static(path.join(__dirname, '..', 'build')))
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
